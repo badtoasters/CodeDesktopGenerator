9665f9a165f77c6c4a2efa457976dc604b61e774
curtis.lowder <clowder2@illinois.edu>
2018-01-17 20:31:22 -0600

    Formatting issue

--- src/curtis/toaster/Blocks/BlockFunctionCall.java
+++ src/curtis/toaster/Blocks/BlockFunctionCall.java
@@ -9,68 +9,68 @@ import java.util.Random;
  * Created by Badtoasters on 12/20/2016.
  */
 public class BlockFunctionCall extends Block {
-    private Variable callingVariable;
-    private String function;
-    private boolean newVariable = false;
-    private ArrayList<Variable> parameterVariables = new ArrayList<>();
+	private Variable callingVariable;
+	private String function;
+	private boolean newVariable = false;
+	private ArrayList<Variable> parameterVariables = new ArrayList<>();
 
-    public BlockFunctionCall() {
-        super(0);
-    }
+	public BlockFunctionCall() {
+		super(0);
+	}
 
-    @Override
-    public void randomize() {
-        // selects random object
-        Random gen = new Random();
-        int i = gen.nextInt(getVariables().size());
-        setCallingVariable(getVariables().get(i));
-        newVariable = false;
+	@Override
+	public void randomize() {
+		// selects random object
+		Random gen = new Random();
+		int i = gen.nextInt(getVariables().size());
+		setCallingVariable(getVariables().get(i));
+		newVariable = false;
 
-        int numParams = gen.nextInt(4);
-        for ( int num = 0 ; num < numParams ; num++ ) {
-            i = gen.nextInt(getVariables().size());
-            Variable toAdd = getVariables().get(i);
-            if ( !parameterVariables.contains( toAdd )) {
-                parameterVariables.add(getVariables().get(i));
-            }
-        }
+		int numParams = gen.nextInt(4);
+		for (int num = 0; num < numParams; num++) {
+			i = gen.nextInt(getVariables().size());
+			Variable toAdd = getVariables().get(i);
+			if (!parameterVariables.contains(toAdd)) {
+				parameterVariables.add(getVariables().get(i));
+			}
+		}
 
-        // Selects random function name
-        setFunction(NameGenerator.getRandomMethodName());
-    }
+		// Selects random function name
+		setFunction(NameGenerator.getRandomMethodName());
+	}
 
-    public Variable getCallingVariable() {
-        return callingVariable;
-    }
+	public Variable getCallingVariable() {
+		return callingVariable;
+	}
 
-    public void setCallingVariable(Variable callingVariable) {
-        this.callingVariable = callingVariable;
-    }
+	public void setCallingVariable(Variable callingVariable) {
+		this.callingVariable = callingVariable;
+	}
 
-    public String getFunction() {
-        return function;
-    }
+	public String getFunction() {
+		return function;
+	}
 
-    public void setFunction(String function) {
-        this.function = function;
-    }
+	public void setFunction(String function) {
+		this.function = function;
+	}
 
-    @Override
-    public String toString() {
-        String beginning = "";
+	@Override
+	public String toString() {
+		String beginning = "";
 
-        if ( callingVariable.getType() == Type.Type_object ) {
-            beginning += callingVariable.getName() + ".";
-        } else {
-            beginning += callingVariable.getName() + " = ";
-        }
-        String end = tab() + beginning + function + "(";
+		if (callingVariable.getType() == Type.Type_object) {
+			beginning += callingVariable.getName() + ".";
+		} else {
+			beginning += callingVariable.getName() + " = ";
+		}
+		String end = tab() + beginning + function + "(";
 
-        for (int var = 0; var < parameterVariables.size() ; var ++ ) {
-            end += (var > 0?" ":"") + parameterVariables.get(var).getName() +
-                    (var == parameterVariables.size() - 1 ? "":" ,");
-        }
-        end += ");";
-        return end;
-    }
+		for (int var = 0; var < parameterVariables.size(); var++) {
+			end += (var > 0 ? " " : "") + parameterVariables.get(var).getName() +
+					(var == parameterVariables.size() - 1 ? "" : ",");
+		}
+		end += ");";
+		return end;
+	}
 }
